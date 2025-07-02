246695138d97cf351eece40abab9dd5f96ad8518
Dimitrij <kvarkas@gmail.com>
2025-07-03 00:31:56 +0100

    upd description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,9 @@
 </script>
 
 <section class="d-flex flex-column align-items-center flex-grow-1 justify-content-center" style="min-height:0; flex: 1 0 auto;">
-	<p>mRemoteNG is a fork of mRemote: an open source, tabbed, multi-protocol, remote connections manager for Windows. </p>
-	<p>mRemoteNG adds bug fixes and new features to mRemote and allows you to view all of your remote connections in a simple yet powerful tabbed interface.</p>
+	<p></p>
+	<p>mRemoteNG (Multi-Remote Next Generation), abbreviated as mR, is an open-source, tabbed remote connections manager for Windows.</p>
+	<p>It allows you to view all of your remote connections in a simple yet powerful interface and supports multiple protocols including RDP, VNC, SSH, HTTP/S and more.</p>
 	<p>
 		<img
 			src="{base}/images/lightbox.png"
