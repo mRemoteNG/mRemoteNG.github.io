37443734626fa74b3d31fc64a5422d4fe03e4544
Dimitrij <kvarkas@gmail.com>
2025-07-03 00:23:37 +0100

    fix for size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,14 @@
 	import { base } from '$app/paths';
 </script>
 
-<section class="d-flex flex-column align-items-center">
+<section class="d-flex flex-column align-items-center flex-grow-1 justify-content-center" style="min-height:0; flex: 1 0 auto;">
 	<p>mRemoteNG is a fork of mRemote: an open source, tabbed, multi-protocol, remote connections manager for Windows. </p>
 	<p>mRemoteNG adds bug fixes and new features to mRemote and allows you to view all of your remote connections in a simple yet powerful tabbed interface.</p>
 	<p>
 		<img
 			src="{base}/images/lightbox.png"
 			alt="Screenshot of mRemoteNG interface"
-			style="max-width:800px;width:100%;height:auto;display:block;margin:0 auto;"
+			style="max-width:1100px;width:100%;height:auto;display:block;margin:0 auto;"
 			loading="lazy"
 		>
 	</p>
