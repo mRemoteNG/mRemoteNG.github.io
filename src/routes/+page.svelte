adf868ff110980998e4e63017d57fedcf9ee9da3
Dimitrij <kvarkas@gmail.com>
2025-07-02 23:59:13 +0100

    fix image size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,13 @@
 </script>
 
 <section class="d-flex flex-column align-items-center">
-	
 	<p>mRemoteNG is a fork of mRemote: an open source, tabbed, multi-protocol, remote connections manager for Windows. </p>
 	<p>mRemoteNG adds bug fixes and new features to mRemote and allows you to view all of your remote connections in a simple yet powerful tabbed interface.</p>
 	<p>
 		<img
 			src="{base}/images/lightbox.png"
 			alt="Screenshot of mRemoteNG interface"
-			style="max-width:100%;height:auto;display:block;margin:0 auto;"
+			style="max-width:600px;width:100%;height:auto;display:block;margin:0 auto;"
 		>
 	</p>
-
 </section>
