8c1192129998dc86872605aac3c805ff0a76324a
Dimitrij <kvarkas@gmail.com>
2025-07-02 23:54:19 +0100

    make image responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,12 @@
 	
 	<p>mRemoteNG is a fork of mRemote: an open source, tabbed, multi-protocol, remote connections manager for Windows. </p>
 	<p>mRemoteNG adds bug fixes and new features to mRemote and allows you to view all of your remote connections in a simple yet powerful tabbed interface.</p>
-	<p><img src="{base}/images/lightbox.png" width="100%" alt="Screenshot of mRemoteNG interface"></p>
+	<p>
+		<img
+			src="{base}/images/lightbox.png"
+			alt="Screenshot of mRemoteNG interface"
+			style="max-width:100%;height:auto;display:block;margin:0 auto;"
+		>
+	</p>
 
 </section>
