85157ed08f2b303c4e2e29bc2b5fed0e8b04fb8e
Dimitrij <kvarkas@gmail.com>
2025-07-03 14:34:40 +0100

    integrity and style fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,52 +33,28 @@
 	<link rel="apple-touch-icon" sizes="180x180" href="{assets}/favicon/apple-touch-icon.png" />
 	<link rel="manifest" href="{assets}/favicon/site.webmanifest" />
 	
-	<!-- Bootstrap CSS -->
-	<link
-		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-		integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-		crossorigin="anonymous" />
-
 	<!-- Bootswatch: Darkly theme -->
 	<link
 		rel="stylesheet"
 		href="https://bootswatch.com/5/darkly/bootstrap.css"
-		integrity="sha384-IznVmLnW7mND5pWWovGJ4PPSv6P0ocjj3pNmKng15+gDLrCYYyeRHqmbq26oqLUK"
-		crossorigin="anonymous" />
+		integrity="sha384-mXM+8P9BUwTXMBS6pzZaBva0g/sZsdn7ZdBimWWpF/1K2kVhkyjQrBznOTf31ui7"
+		crossorigin="anonymous" 
+	/>
 
 	<!-- Bootstrap Icons -->
-	<link
-		rel="stylesheet"
-		href="https://bootswatch.com/_vendor/bootstrap-icons/font/bootstrap-icons.min.css"
-		integrity="sha384-aMbNX8jFtSKhIuWSIHn8kuZ+XZ0IMKhpHeElS5OpN9SoI+TAcG7byPLj4UOpAIYn"
-		crossorigin="anonymous" />
-
-	<!-- PrismJS Theme -->
-	<link
-		rel="stylesheet"
-		href="https://bootswatch.com/_vendor/prismjs/themes/prism-okaidia.css"
-		integrity="sha384-6U+XhAWXGaFaDfnWJhU8cfYUIRka9j/qcWDCUcGjA9fs4yiMsCyKrhceV6QOBwv8"
-		crossorigin="anonymous" />
-
-	<!-- Bootswatch Custom -->
-	<link
-		rel="stylesheet"
-		href="https://bootswatch.com/_assets/css/custom.min.css"
-		integrity="sha384-Nyxm9Mvo3fMRrFnaIt9shtDFdlq2MufOlaSPadnStOsUKTSlyvqa7X2SlP7UNXoD"
-		crossorigin="anonymous" />
+	<link 
+		rel="stylesheet" 
+		href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" 
+		integrity="sha384-tViUnnbYAV00FLIhhi3v/dWt3Jxw4gZQcNoSCxCIFNJVCx7/D55/wXsrNIRANwdD" 
+		crossorigin="anonymous"
+	/>
 
 	<!-- Bootstrap JS -->
-	<script
-		src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-		integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
-		crossorigin="anonymous"></script>
-
-	<!-- Bootswatch JS Bundle -->
-	<script
-		src="https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-LsTGcbITkMbUTw6RjDAkhn5UBOJgqN35N2YdYoZyOAtn9WEuJOnzq+JEmzhlDyIU"
-		crossorigin="anonymous"></script>
+	<script 
+		src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js"
+		integrity="sha256-lel57Jim1/CW4I1iEkaHexK/J9h/ZRneeORKiQuNOIg="
+		crossorigin="anonymous">
+	</script>
 </svelte:head>
 
 <div class="app-layout container">
