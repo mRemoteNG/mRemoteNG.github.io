273f91af4caaae755a3a00cf8d971477439a083f
Dimitrij <kvarkas@gmail.com>
2025-07-08 23:21:30 +0100

    upd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -65,13 +65,7 @@
 	/>
 
 
-	<!-- Bootstrap Icons -->
-	<link 
-		rel="stylesheet" 
-		href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" 
-		integrity="sha384-tViUnnbYAV00FLIhhi3v/dWt3Jxw4gZQcNoSCxCIFNJVCx7/D55/wXsrNIRANwdD" 
-		crossorigin="anonymous"
-	/>
+	
 	<!-- Preload for faster execution -->
   	<link 
 		rel="preload" 
